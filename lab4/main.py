34a1e0ab278d3033a80d2c7b9943fc0ec8d2b075
cocahonka <cocahonka@gmail.com>
2024-09-23 18:35:50 +0300

    update main for handle gnome sort

--- lab4/main.py
+++ lab4/main.py
@@ -1,14 +1,21 @@
 from itertools import islice as take
 from typing import Callable, Dict
 
+from lab2.linked_list.doubly_linked_list import DoublyLinkedList
+from lab2.linked_list.linked_list import ILinkedList
 from lab4.algs.arrays.insertion_sort import insertion_sort
 from lab4.algs.arrays.merge_sort import merge_sort, merge_sort_in_place
+from lab4.algs.linked_list.gnome_sort import (
+    gnome_sort_through_node,
+    gnome_sort_through_public_api,
+)
 from lab4.arrays.array import IArray
 from lab4.arrays.dynamic_array import DynamicArray
 from lab4.models.book import Book, book_generator
 
 Comparator = Callable[[Book, Book], bool]
 ArraySortFunction = Callable[[IArray[Book], Comparator], None]
+LinkedListSortFunction = Callable[[ILinkedList[Book], Comparator], None]
 
 
 def array_sort_example(
@@ -30,7 +37,27 @@ def array_sort_example(
         print(book)
 
 
+def linked_list_sort_example(
+    sort_function: LinkedListSortFunction,
+    comparator: Comparator,
+    criteria: str,
+) -> None:
+    books: ILinkedList[Book] = DoublyLinkedList()
+    [books.add(book) for book in take(book_generator(), 10)]
+
+    print("Before sorting:")
+    for book in books:
+        print(book)
+    print()
+
+    sort_function(books, comparator)
+    print(f"After sorting (by {criteria}):")
+    for book in books:
+        print(book)
+
+
 def main() -> None:
+    # TODO: Fix type hinting for gnome_sort_through_node
     functions: Dict[str, Callable[..., object]] = {
         "Insertion sort": lambda: array_sort_example(
             insertion_sort,
@@ -46,6 +73,16 @@ def main() -> None:
             merge_sort_in_place,
             lambda a, b: a.author > b.author,
             "descending author",
+        ),
+        "Gnome sort (public API)": lambda: linked_list_sort_example(
+            gnome_sort_through_public_api,
+            lambda a, b: a.author < b.author,
+            "ascending author",
+        ),
+        "Gnome sort (through node)": lambda: linked_list_sort_example(
+            gnome_sort_through_node,  # type: ignore
+            lambda a, b: a.author < b.author,
+            "ascending author",
         ),
     }
 
