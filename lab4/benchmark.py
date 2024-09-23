2c6869592914b0c1919bb403ae336d5572b505a7
cocahonka <cocahonka@gmail.com>
2024-09-23 11:20:10 +0300

    add stub files for lab4

---
+++ lab4/benchmark.py
@@ -0,0 +1,5 @@
+from common import benchmark
+from common.benchmark import *
+
+if __name__ == "__main__":
+    benchmark.main()
---
+++ lab4/main.py
@@ -0,0 +1,5 @@
+def main() -> None: ...
+
+
+if __name__ == "__main__":
+    main()
