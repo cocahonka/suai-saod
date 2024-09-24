32837c41bf3dd81c5a103a9e8bc22016dae4a9ac
cocahonka <cocahonka@gmail.com>
2024-09-24 15:37:23 +0300

    refactor lab4 benchmark

--- lab4/benchmark.py
+++ lab4/benchmark.py
@@ -8,7 +8,10 @@ from lab2.linked_list.doubly_linked_list import DoublyLinkedList
 from lab4.algs.arrays.insertion_sort import insertion_sort
 from lab4.algs.arrays.merge_sort import merge_sort, merge_sort_in_place
 from lab4.algs.linked_list.counting_sort import counting_sort_through_public_api
-from lab4.algs.linked_list.gnome_sort import gnome_sort_through_public_api
+from lab4.algs.linked_list.gnome_sort import (
+    gnome_sort_through_node,
+    gnome_sort_through_public_api,
+)
 from lab4.arrays.array import IArray
 from lab4.arrays.dynamic_array import DynamicArray
 
@@ -17,7 +20,7 @@ class DynamicArrayBenchmark(Benchmark):
     @override
     def setUp(self) -> None:
         self.array: IArray[int] = DynamicArray()
-        self.n = 100000
+        self.n = 2000
 
     def benchmark_add(self) -> BenchmarkCallback:
         def callback() -> None:
@@ -34,8 +37,7 @@ class DynamicArrayBenchmark(Benchmark):
         return callback, self.n
 
     def benchmark_remove(self) -> BenchmarkCallback:
-        n: int = 2000
-        random_values: List[int] = sample(range(n), n)
+        random_values: List[int] = sample(range(self.n), self.n)
         self.array.add_all(random_values)
         index: int = 0
 
@@ -44,14 +46,14 @@ class DynamicArrayBenchmark(Benchmark):
             self.array.remove(random_values[index])
             index += 1
 
-        return callback, n
+        return callback, self.n
 
 
 class SortingBenchmark(Benchmark):
     @override
     def setUp(self) -> None:
-        self.array_n = 10000
-        self.linked_list_n = 500
+        self.array_n = 1000
+        self.linked_list_n = 1000
 
         self.array: List[int] = sample(range(self.array_n), self.array_n)
 
@@ -62,31 +64,37 @@ class SortingBenchmark(Benchmark):
         def callback() -> None:
             insertion_sort(self.array)
 
-        return callback, 1
+        return callback, 1, self.array_n
 
     def benchmark_merge_sort(self) -> BenchmarkCallback:
         def callback() -> None:
             merge_sort(self.array)
 
-        return callback, 1
+        return callback, 1, self.array_n
 
     def benchmark_merge_sort_in_place(self) -> BenchmarkCallback:
         def callback() -> None:
             merge_sort_in_place(self.array)
 
-        return callback, 1
+        return callback, 1, self.array_n
 
     def benchmark_counting_sort(self) -> BenchmarkCallback:
         def callback() -> None:
-            counting_sort_through_public_api(self.linked_list, key=lambda x: x)
+            counting_sort_through_public_api(self.linked_list, lambda x: x)
 
-        return callback, 1
+        return callback, 1, self.linked_list_n
 
-    def benchmark_gnome_sort(self) -> BenchmarkCallback:
+    def benchmark_gnome_sort_through_public_api(self) -> BenchmarkCallback:
         def callback() -> None:
             gnome_sort_through_public_api(self.linked_list)
 
-        return callback, 1
+        return callback, 1, self.linked_list_n
+
+    def benchmark_gnome_sort_through_node(self) -> BenchmarkCallback:
+        def callback() -> None:
+            gnome_sort_through_node(self.linked_list)
+
+        return callback, 1, self.linked_list_n
 
 
 if __name__ == "__main__":
