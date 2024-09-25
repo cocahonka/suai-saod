51a0b18b72e38be8da25ea9f0880779cb2f1dcfd
cocahonka <cocahonka@gmail.com>
2024-09-25 13:50:50 +0300

    implement basic fibonacci search

--- lab5/algs/fibonacci_search.py
+++ lab5/algs/fibonacci_search.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from common.comparable import default_compare_to
+from lab2.linked_list.doubly_linked_list import DoublyLinkedList
 from lab5.main import C, Comparator, KeySelector, SearchSequence, T
 
 
@@ -9,12 +10,10 @@ def fibonacci_search(
     target: C,
     compare: Comparator[C] = default_compare_to,
 ) -> int:
-    return fibonacci_search_by(
-        sequence,
-        target,
-        lambda x: x,
-        compare,
-    )
+    if isinstance(sequence, DoublyLinkedList):
+        return _fibonacci_search_nodes(sequence, target, lambda x: x, compare)
+
+    return fibonacci_search_by(sequence, target, lambda x: x, compare)
 
 
 def fibonacci_search_by(
@@ -23,4 +22,37 @@ def fibonacci_search_by(
     key_selector: KeySelector[T, C],
     compare: Comparator[C] = default_compare_to,
 ) -> int:
-    raise NotImplemented
+    fib, fib_1, fib_2, n = 1, 1, 0, len(sequence)
+
+    while n > fib:
+        fib, fib_1, fib_2 = fib + fib_1, fib, fib_1
+
+    offset: int = -1
+
+    while fib > 1:
+        index: int = min(offset + fib_2, n - 1)
+
+        compare_result = compare(key_selector(sequence[index]), target)
+
+        if compare_result == 0:
+            return index
+
+        if compare_result < 0:
+            offset = index
+            fib, fib_1, fib_2 = fib_1, fib_2, fib_1 - fib_2
+        else:
+            fib, fib_1, fib_2 = fib_2, fib_1 - fib_2, fib_2 - (fib_1 - fib_2)
+
+    if offset + 1 < n and compare(key_selector(sequence[offset + 1]), target) == 0:
+        return offset + 1
+
+    return -1
+
+
+def _fibonacci_search_nodes(
+    sequence: DoublyLinkedList[T],
+    target: C,
+    key_selector: KeySelector[T, C],
+    compare: Comparator[C] = default_compare_to,
+) -> int:
+    return fibonacci_search_by(sequence, target, key_selector, compare)
