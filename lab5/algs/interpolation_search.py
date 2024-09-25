39db0d10f772d5a164a1c8ab7748bd31bbf0e817
cocahonka <cocahonka@gmail.com>
2024-09-25 22:23:35 +0300

    add special implementation for linked list in interpolation search

--- lab5/algs/interpolation_search.py
+++ lab5/algs/interpolation_search.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TypeVar, Union
 
 from common.comparable import default_compare_to
-from lab2.linked_list.doubly_linked_list import DoublyLinkedList
+from lab2.linked_list.doubly_linked_list import DoubleNode, DoublyLinkedList
 from lab5.main import Comparator, KeySelector, SearchSequence, T
 
 N = TypeVar("N", bound=Union[int, float])
@@ -61,4 +61,51 @@ def _interpolation_search_nodes(
     key_selector: KeySelector[T, N],
     compare: Comparator[N] = default_compare_to,
 ) -> int:
-    return interpolation_search_by(sequence, target, key_selector, compare)
+    if sequence._head is None or sequence._tail is None:
+        return -1
+
+    low, high = 0, len(sequence) - 1
+    head: DoubleNode[T] = sequence._head
+    tail: DoubleNode[T] = sequence._tail
+
+    while (
+        compare(sequence_low := key_selector(head.value), target) <= 0
+        and compare(sequence_high := key_selector(tail.value), target) >= 0
+        and compare(sequence_low, sequence_high) != 0
+    ):
+        ratio: float = (target - sequence_low) / (sequence_high - sequence_low)  # type: ignore[operator]
+        position: int = low + int(ratio * (high - low))
+
+        steps_from_head: int = position - low
+        steps_from_tail: int = high - position
+        node: DoubleNode[T] = head if steps_from_head < steps_from_tail else tail
+
+        if steps_from_head < steps_from_tail:
+            while steps_from_head > 0:
+                assert node.next is not None
+                node = node.next
+                steps_from_head -= 1
+        else:
+            while steps_from_tail > 0:
+                assert node.prev is not None
+                node = node.prev
+                steps_from_tail -= 1
+
+        compare_result: int = compare(key_selector(node.value), target)
+
+        if compare_result == 0:
+            return position
+
+        if compare_result < 0:
+            low = position + 1
+            assert node.next is not None
+            head = node.next
+        else:
+            high = position - 1
+            assert node.prev is not None
+            tail = node.prev
+
+    if compare(key_selector(head.value), target) == 0:
+        return low
+
+    return -1
