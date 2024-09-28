40687bf640df69fba40f8079a6c1cfec9204fc1f
cocahonka <cocahonka@gmail.com>
2024-09-28 18:33:19 +0300

    add graph algorithms benchmark

--- lab6/benchmark.py
+++ lab6/benchmark.py
@@ -4,6 +4,8 @@ from typing import List
 from common import benchmark
 from common.benchmark import Benchmark, BenchmarkCallback
 from common.extra_typing import override
+from lab6.algs.adjacency_matrix.dijkstra import dijkstra
+from lab6.algs.adjacency_matrix.topological_sort import topological_sort
 from lab6.graph.adjacency_matrix_graph import AdjacencyMatrixGraph
 from lab6.graph.graph import GraphTraversalType
 from lab6.serializers.graph_serializer import GraphSerializer
@@ -117,7 +119,46 @@ class GraphHydratedBenchmark(Benchmark):
         return callback, 1, self.n
 
 
-class GraphAlgsBenchmark(Benchmark): ...
+class GraphAlgsBenchmark(Benchmark):
+    @override
+    def setUp(self) -> None:
+        self.n: int = 2500
+
+    def benchmark_directed_dijkstra(self) -> BenchmarkCallback:
+        directed_graph: AdjacencyMatrixGraph[int, int] = AdjacencyMatrixGraph(is_directed=True)
+        directed_graph.add_all(sample(range(self.n), self.n))
+
+        def callback() -> None:
+            dijkstra(directed_graph, randint(0, self.n - 1), randint(0, self.n - 1))
+
+        return callback, self.n
+
+    def benchmark_undirected_dijkstra(self) -> BenchmarkCallback:
+        undirected_graph: AdjacencyMatrixGraph[int, int] = AdjacencyMatrixGraph(is_directed=False)
+        undirected_graph.add_all(sample(range(self.n), self.n))
+
+        def callback() -> None:
+            dijkstra(undirected_graph, randint(0, self.n - 1), randint(0, self.n - 1))
+
+        return callback, self.n
+
+    def benchmark_topological_sort(self) -> BenchmarkCallback:
+        elements_count: int = 100
+        directed_graphs: List[AdjacencyMatrixGraph[int, int]] = [
+            AdjacencyMatrixGraph(is_directed=True) for _ in range(self.n)
+        ]
+
+        for directed_graph in directed_graphs:
+            directed_graph.add_all(sample(range(elements_count), elements_count))
+
+        index: int = 0
+
+        def callback() -> None:
+            nonlocal index
+            topological_sort(directed_graphs[index])
+            index += 1
+
+        return callback, self.n, elements_count
 
 
 if __name__ == "__main__":
