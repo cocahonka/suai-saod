5052f1c526830ab3d63c0cb17217f7b736a10c2b
cocahonka <cocahonka@gmail.com>
2024-09-30 18:43:31 +0300

    fix missing isolated vertices after graph serialization

--- lab6/serializers/graph_serializer.py
+++ lab6/serializers/graph_serializer.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 import json
-from typing import Any, Dict, List, Optional, Tuple, Type, TypeVar, cast
+from typing import Any, Dict, List, Optional, Tuple, Type, TypeVar
 
-from lab6.graph.graph import IGraph, Weight
+from lab6.graph.graph import GraphTraversalType, IGraph, Weight
 
 T = TypeVar("T")
 
@@ -12,10 +12,19 @@ class GraphSerializer:
     @classmethod
     def save_graph_to_file(cls, graph: IGraph[T, Weight], filename: str) -> None:
         with open(filename, "w", encoding="utf-8") as file:
+            vertices: List[T] = [*graph.generator(traverse_type=GraphTraversalType.BREADTH_FIRST)]
             edges: List[Tuple[T, T, Optional[Weight]]] = graph.get_edges()
 
             data: Dict[str, Any] = {
-                "edges": [[cls.node_to_dict(u), cls.node_to_dict(v), w] for u, v, w in edges]
+                "vertices": [cls.node_to_dict(vertex) for vertex in vertices],
+                "edges": [
+                    [
+                        cls.node_to_dict(from_vertex),
+                        cls.node_to_dict(to_vertex),
+                        weight,
+                    ]
+                    for from_vertex, to_vertex, weight in edges
+                ],
             }
 
             json.dump(data, file, ensure_ascii=False, indent=4)
@@ -28,19 +37,22 @@ class GraphSerializer:
         data_type: Type[T],
     ) -> None:
         graph.clear()
+
         with open(filename, "r", encoding="utf-8") as file:
             data: Dict[str, Any] = json.load(file)
-            edges: List[Tuple[T, T, Optional[Weight]]] = [
-                (cls.dict_to_node(u, data_type), cls.dict_to_node(v, data_type), w)
-                for u, v, w in data["edges"]
+
+            vertices: List[T] = [cls.dict_to_node(vertex, data_type) for vertex in data["vertices"]]
+            edges: List[Tuple[T, T, Weight]] = [
+                (
+                    cls.dict_to_node(from_vertex, data_type),
+                    cls.dict_to_node(to_vertex, data_type),
+                    weight,
+                )
+                for from_vertex, to_vertex, weight in data["edges"]
             ]
 
-        for u, v, _ in edges:
-            graph.add(u)
-            graph.add(v)
-
-        for u, v, w in edges:
-            graph.connect(u, v, cast(Weight, w))
+        graph.add_all(vertices)
+        graph.connect_all(edges)
 
     @classmethod
     def node_to_dict(cls, node: T) -> object:
